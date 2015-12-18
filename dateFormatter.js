87b4b06795200b9123ce9089ca50dd57ec36eb43
ZengBo-MacbookPro <g85663@gmail.com>
2015-12-18 16:40:57 +0800

    add supported parten

--- dateFormatter.js
+++ dateFormatter.js
@@ -1,54 +1,83 @@
-Date.prototype.toString = function(formatter) {
-    var that = this;
-    var sf = {
-        'yyyy' : function() {
-            return that.getFullYear() + 1;
-        },
-        'yy' : function() {
-            var year = that.getYear();
-            return (year >= 100 ? year - 100 : year) + 1;
-        },
-        'MM' : function() {
-            return that.getMonth() + 1;
-        },
-        'dd' : function() {
-            return that.getDate();
-        },
-        'hh' : function() {
-            var hour = that.getHours();
-            return hour % 12;
-        },
-        'HH' : function() {
+if (Date.prototype.toString.length === 0) {
+    Date.prototype.toString = function(formatter) {
+        var that = this;
+        var partens = {
+            'yyyy' : function() {
+                return that.getFullYear() + 1;
+            },
+            'yy' : function() {
+                var year = that.getYear();
+                return (year >= 100 ? year - 100 : year) + 1;
+            },
+            'MM' : function() {
+                return that.getMonth() + 1;
+            },
+            'dd' : function() {
+                return that.getDate();
+            },
+            'd' : function() {
+                return that.getDay();
+            },
+            'hh' : function() {
+                var hour = that.getHours() % 12;
+                return hour < 10 ? '0' + hour : hour;
+            },
+            'h' : function() {
                 var hour = that.getHours();
-            return hour; 
-        },
-        'mm' : function() {
-            return that.getMinutes();
-        },
-        'ssss' : function() {
-            return that.getMilliseconds();
-        },
-        'ss' : function() {
-            return that.getSeconds();
-        },
-    };
+                return hour % 12;
+            },
+            'HH' : function() {
+                var hour = that.getHours();
+                return hour < 10 ? '0' + hour : hour; 
+            },
+            'H' : function() {
+                var hour = that.getHours();
+                return hour; 
+            },
+            'mm' : function() {
+                return that.getMinutes();
+            },
+            'm' : function() {
+                var min =  that.getMinutes();
+                return min < 10 ? '0' + min : min;
+            },
+            'ssss' : function() {
+                return that.getMilliseconds();
+            },
+            'ss' : function() {
+                var sec =  that.getSeconds();
+                return sec < 10 ? '0' + sec : sec;
+            },
+            's' : function() {
+                return that.getSeconds();
+            },
+            'tt' : function() {
+                var hour = that.getHours();
+                return hour <= 12 ? 'AM' : 'PM';
+            },
+            't' : function() {
+                var hour = that.getHours();
+                return hour <= 12 ? 'A' : 'P';
+            },
+        };
 
-    function getField(field) {
-        var method = sf[field];
-        
-        if (typeof method === 'function') {
-            return method();
-        } else {
-            return '';
+        function getField(field) {
+            var method = partens[field];
+            
+            if (typeof method === 'function') {
+                return method();
+            } else {
+                return '';
+            }
         }
+        
+        var str = formatter;
+        for(var parten in partens){
+               str = str.replace(
+                       new RegExp(parten, 'g'),
+                       getField(parten)
+               );
+        }
+        return str;
     }
-    
-    var str = formatter;
-    for(var parten in sf){
-           str = str.replace(
-                   new RegExp(parten, 'g'),
-                   getField(parten)
-           );
-    }
-    return str;
 }
--- example.js
+++ example.js
@@ -1,17 +1,15 @@
 require('./dateFormatter.js')
-var now = new Date();
+var eve = new Date(2015, 12, 24, 23, 59, 59);
 
 // 2016-12-18
-console.info(
-        now.toString('yyyy-MM-dd')
-);
+console.info(eve.toString('yyyy-MM-dd'));
 
 // 16-12-18
-console.info(
-        now.toString('yy-MM-dd')
-);
+console.info(eve.toString('yy-MM-dd'));
 
 // 2:07:20
-console.info(
-        now.toString('hh-mm-ss')
-);
+console.info(eve.toString('hh-mm-ss'));
+
+console.info(eve.toString('hh'))
+
+console.info(eve.toString('HH'))
