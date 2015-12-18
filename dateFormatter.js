8644f1986298f7dc61000e6b475279354ead79e3
ZengBo-MacbookPro <g85663@gmail.com>
2015-12-18 14:48:23 +0800

    support hh HH mm ss sss

--- dateFormatter.js
+++ dateFormatter.js
@@ -1,19 +1,54 @@
 Date.prototype.toString = function(formatter) {
     var that = this;
     var sf = {
-        'yyyy' : 'getFullYear',
-        'MM' : 'getMonth',
-        'dd' : 'getDate'
+        'yyyy' : function() {
+            return that.getFullYear() + 1;
+        },
+        'yy' : function() {
+            var year = that.getYear();
+            return (year >= 100 ? year - 100 : year) + 1;
+        },
+        'MM' : function() {
+            return that.getMonth() + 1;
+        },
+        'dd' : function() {
+            return that.getDate();
+        },
+        'hh' : function() {
+            var hour = that.getHours();
+            return hour % 12;
+        },
+        'HH' : function() {
+                var hour = that.getHours();
+            return hour; 
+        },
+        'mm' : function() {
+            return that.getMinutes();
+        },
+        'ssss' : function() {
+            return that.getMilliseconds();
+        },
+        'ss' : function() {
+            return that.getSeconds();
+        },
     };
 
     function getField(field) {
-        var pre = field === 'dd' ? 0 : 1;
         var method = sf[field];
-        var str = that[method]() + pre + '';
-        return str;
+        
+        if (typeof method === 'function') {
+            return method();
+        } else {
+            return '';
+        }
     }
-
-    return formatter.replace(/yyyy/, getField('yyyy'))
-        .replace(/MM/, getField('MM'))
-        .replace(/dd/, getField('dd'))
+    
+    var str = formatter;
+    for(var parten in sf){
+           str = str.replace(
+                   new RegExp(parten, 'g'),
+                   getField(parten)
+           );
+    }
+    return str;
 }
--- example.js
+++ example.js
@@ -1,5 +1,17 @@
 require('./dateFormatter.js')
 var now = new Date();
+
+// 2016-12-18
 console.info(
         now.toString('yyyy-MM-dd')
+);
+
+// 16-12-18
+console.info(
+        now.toString('yy-MM-dd')
+);
+
+// 2:07:20
+console.info(
+        now.toString('hh-mm-ss')
 );
