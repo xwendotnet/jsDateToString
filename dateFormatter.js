0093652a512a15f9a0fbabb158baac4327c3aab7
ZengBo-MacbookPro <g85663@gmail.com>
2015-12-18 11:01:03 +0800

    support yyyy MM dd

---
+++ dateFormatter.js
@@ -0,0 +1,22 @@
+Date.prototype.toString = function(formatter) {
+    var that = this;
+    var sf = {
+        'yyyy' : 'getFullYear',
+        'MM' : 'getMonth',
+        'dd' : 'getDate'
+    };
+
+    function getField(field) {
+        var pre = field === 'dd' ? 0 : 1;
+        var method = sf[field];
+        var str = that[method]() + pre + '';
+        return str;
+    }
+
+    return formatter.replace(/yyyy/, getField('yyyy'))
+        .replace(/MM/, getField('MM'))
+        .replace(/dd/, getField('dd'))
+}
+
+var d = new Date();
+console.info(d.toString('yyyy-MM-dd'));
